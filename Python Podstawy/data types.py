08aa49e45281c45cdce93ec14a3ddbeef3222138
Tigreshka <tigreshkas@gmail.com>
2021-02-17 22:28:48 +0100

    add python data types info

---
+++ Python Podstawy/data types.py
@@ -0,0 +1,17 @@
+# int - całkowita
+print(0, 1, -3, 128, 454654654, 0b111, 0x18c)
+
+# float - rzeczywista
+print(0.123, 256.2, -3.14, 1e10)
+
+# complex - zespolona
+print(1-2j, 30+15j, 3j) # s.real - część rzeczywista, s.imag - część urojona
+
+# str - stringi
+print("napis", "tez!@#$^napis", 'pojedyncze apostrofy')
+
+# bool - typy logiczne
+print(True, False)
+
+# nonetype
+print(None)

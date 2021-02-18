ea1eedd99f5caae9453d938169d53c56098072d7
Tigreshka <tigreshkas@gmail.com>
2021-02-18 22:02:36 +0100

    add more or less version 2

---
+++ Python Podstawy/more_or_less_v.2.0.py
@@ -0,0 +1,12 @@
+# Version 2
+
+a = input("Enter first number")
+b = input("Enter second number")
+
+if a < b:
+    print(a, b)
+else:
+    if a > b:
+        print(b, a)
+    else:
+        print("Same")

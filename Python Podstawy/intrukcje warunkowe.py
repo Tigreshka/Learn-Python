436adf4136b00ecc36454d2d685e0c096362281e
Tigreshka <tigreshkas@gmail.com>
2021-02-18 21:56:12 +0100

    add instrukcje warunkowe oraz zadanie parzyste nieparzyste

---
+++ Python Podstawy/intrukcje warunkowe.py
@@ -0,0 +1,17 @@
+a = 10
+b = 10
+if a == b:
+    print("prawda")
+    print("prawda, prawda")
+
+print("zawsze")
+
+
+
+a = 101
+b = 10111
+if 5 + 5:
+    print("prawda")
+    print("prawda, prawda")
+
+print("zawsze")

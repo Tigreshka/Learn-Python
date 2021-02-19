65a988b123732714a8fff9f84af290b91b56a829
Tigreshka <tigreshkas@gmail.com>
2021-02-19 23:04:18 +0100

    update program, dodano ilosc podanych liczb

--- Python Podstawy/Suma liczb.py
+++ Python Podstawy/Suma liczb.py
@@ -1,8 +1,15 @@
 msg = "podaj liczbe != 0 "
 sum = 0
+numbers = 0
 number = 1
 while number != 0:
     number = int(input(msg))
     print("podałeś :", number)
     sum = sum + number
+    numbers = numbers + 1
+
+# nie licze ostatniego zera
+numbers = numbers - 1
+
 print("Suma liczb to ", sum)
+print("podałeś ", numbers, "liczb(y)")

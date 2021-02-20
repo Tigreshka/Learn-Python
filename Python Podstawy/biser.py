ffb9d1f3106648b7f43e09b13eac278b2b738ef6
Tigreshka <tigreshkas@gmail.com>
2021-02-20 23:04:10 +0100

    add program - zadanie biser

---
+++ Python Podstawy/biser.py
@@ -0,0 +1,10 @@
+# Pudełko zawiera wielokolorowe koraliki (lub koraliki).
+# Wszystkie koraliki mają ten sam kształt, rozmiar i wagę.
+# Koraliki mogą mieć jeden z N różnych kolorów.
+# W pudełku znajduje się wiele koralików w każdym kolorze.
+# Wymagane jest określenie minimalnej liczby koralików,
+# które można wyciągnąć z pudełka bez patrzenia,
+# tak aby wśród nich były gwarantowane dwa koraliki tego samego koloru.
+
+n = int(input("Enter biser color count"))
+print(n+1)
